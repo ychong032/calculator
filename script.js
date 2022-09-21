78197fd243fae6826b3fbfd5f632c54476d4b9e9
Chong Yow Lim <yowlimchong2403@gmail.com>
2022-09-21 13:51:35 +0800

    Enable inputting numbers onto display

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ function divide(a, b) {
     return a / b;
 }
 
+function populateDisplay() {
+
+}
+
 function operate(operator, a, b) {
     switch (operator) {
         case "+":
@@ -28,3 +32,27 @@ function operate(operator, a, b) {
             console.log("Something went wrong in operate()");
     }
 }
+
+function displayInput(e) {
+    if (resultValue === "") {
+        resultValue = e.target.textContent;
+    } else {
+        resultValue = resultValue.concat(e.target.textContent);
+    }
+    result.textContent = resultValue;
+}
+
+function clearDisplay() {
+    expression.textContent = "";
+    result.textContent = "";
+    resultValue = "";
+}
+
+let resultValue = "";
+const result = document.querySelector("#result");
+const expression = document.querySelector("#expression");
+const inputButtons = document.querySelectorAll("button.input");
+const clearButton = document.querySelector("#clear");
+
+inputButtons.forEach(item => item.addEventListener("click", displayInput));
+clearButton.addEventListener("click", clearDisplay); 
